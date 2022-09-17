370d71c453d7c3fb8ce460b0f90810e893eb1890
Shevchencko-Alexander <shevchenckosasha20141998@gmail.com>
2022-09-17 22:16:39 +0300

    added database connection (mongoDB) to the project

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = 5000;
+import express from 'express';
+import mongoose from 'mongoose';
+
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
+   console.log(req.body);
    res.status(200).json('SERVER IS WORKING !!!');
 });
 
-app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+async function startApp() {
+   try {
+      await mongoose.connect(DB_URL, {
+         useUnifiedTopology: true,
+         useNewUrlParser: true,
+      });
+      app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+startApp();
