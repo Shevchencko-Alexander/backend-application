00259dbc27ca9710b2544ef10933b9c3ecb42a1c
Shevchencko-Alexander <shevchenckosasha20141998@gmail.com>
2022-11-26 21:01:16 +0200

    created a 'PostController.js' file

--- router.js
+++ router.js
@@ -1,20 +1,12 @@
 import Router from 'express';
-import Post from './Post.js';
+import PostController from './PostController.js';
 
 const router = new Router();
 
-router.post('/posts', async (req, res) => {
-   try {
-      const { author, title, content, picture } = req.body;
-      const post = await Post.create({ author, title, content, picture });
-      res.status(200).json(post);
-   } catch (error) {
-      res.status(500).json(error);
-   }
-});
-router.post('/posts');
-router.post('/posts/:id');
-router.post('/posts');
-router.post('/posts/:id');
+router.post('/posts', PostController.create);
+router.get('/posts', PostController.getAll);
+router.get('/posts/:id', PostController.getOne);
+router.put('/posts', PostController.update);
+router.delete('/posts/:id', PostController.delete);
 
 export default router;
